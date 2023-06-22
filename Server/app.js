db3b128f10ed91d612468922b5998de52b7e09fa
Simon Atiegar <atiegardelali@gmail.com>
2023-06-22 16:13:31 +0000

    add streams

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,9 @@
 var http = require('http');
 
+
+
+
+/*
 var server = http.createServer(function (req, res) {
     console.log('request was made: ' + req.url);
     res.writeHead(200, { 'content-Type': 'text/plain' });
@@ -8,3 +12,4 @@ var server = http.createServer(function (req, res) {
 
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
+*/ 
